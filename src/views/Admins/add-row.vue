fbd45d11ec736db1fa86e0964a7534500a3081a2
Mohammed Lokma <mesholokma@gmail.com>
2022-12-19 23:09:59 +0200

    last-updates-v1

--- src/views/Admins/add-row.vue
+++ src/views/Admins/add-row.vue
@@ -1,11 +1,13 @@
   
 <template>
+      <validation-observer ref="simpleRules">
     <b-form 
       v-if="!editRow"
      @submit.prevent>
       <b-row>
         <b-col v-for="item in items" :key="item.id" cols="12">
           <b-form-group
+          v-if="item.arabicName !== 'اسم المندوب'"
             :label="item.arabicName"
             :label-for="'v-'+ item.name"
           >
@@ -16,6 +18,34 @@
               :placeholder="item.arabicName"
             />
           </b-form-group>
+           <b-form-group
+           v-else-if="item.arabicName === 'اسم المندوب'"
+            label="اسم المندوب"
+            :label-for="'v-agentName'"
+          >
+           <validation-provider
+                  #default="{ errors }"
+                  name="اسم المندوب"
+                  rules="required"
+                >
+              <b-dropdown
+           style="width:100%"
+      v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+      :text=" agentName==null? 'اسم المندوب': agentName"
+      v-model="data[item.name]"
+      :state="errors.length > 0 ? false:null"
+      variant="outline-primary"
+    >
+      <b-dropdown-item 
+      v-for="agent in agents" 
+      :key="agent.id"
+      :value="agent.name" @click="agentName=agent.name ,data[item.name] = agentName">
+      {{agent.name}}
+      </b-dropdown-item>
+    </b-dropdown>
+     <small class="text-danger">{{ errors[0] }}</small>
+                </validation-provider>
+          </b-form-group>
         </b-col>
 
         <!-- submit and reset -->
@@ -25,7 +55,7 @@
             type="submit"
             variant="primary"
             class="mr-1"
-            @click="AddRow"
+            @click="validationForm"
           >
             إضافة
           </b-button>
@@ -46,6 +76,7 @@
       <b-row>
         <b-col v-for="item in items" :key="item.id" cols="12">
           <b-form-group
+          v-if="item.arabicName !== 'اسم المندوب'"
             :label="item.arabicName"
             :label-for="'v-'+ item.name"
           >
@@ -55,6 +86,28 @@
               v-model="row[item.name]"
               :placeholder="item.arabicName"
             />
+          </b-form-group>
+           <b-form-group
+          
+           v-else-if="item.arabicName === 'اسم المندوب'"
+            label="اسم المندوب"
+            :label-for="'v-agentName'"
+          >
+           
+              <b-dropdown
+               style="width: 100%"
+      v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+      :text="agentName"
+      v-model="row[item.name]"
+      variant="outline-primary"
+    >
+      <b-dropdown-item 
+      v-for="agent in agents" 
+      :key="agent.id"
+      :value="agent.name" @click="row[item.name]= agent.name,agentName=row[item.name]">
+      {{agent.name}}
+      </b-dropdown-item>
+    </b-dropdown>
           </b-form-group>
         </b-col>
          <b-col cols="12">
@@ -78,7 +131,7 @@
             type="submit"
             variant="primary"
             class="mr-1"
-            @click="EditRow"
+            @click="validationForm"
           >
             تعديل
           </b-button>
@@ -93,20 +146,26 @@
         </b-col>
       </b-row>
     </b-form>
+      </validation-observer>
   </template>
   
   <script>
   import {
-    BRow, BCol, BFormGroup, BFormInput, BFormCheckbox, BForm, BButton,
+    BRow, BCol, BFormGroup, BFormInput, BFormCheckbox,BDropdown, BDropdownItem, BForm, BButton,
   } from 'bootstrap-vue'
   import Ripple from 'vue-ripple-directive'
-  
+import { ValidationProvider, ValidationObserver, localize } from 'vee-validate'
+  import { required } from '@validations'
   export default {
     components: {
+      ValidationProvider,
+      ValidationObserver,
       BRow,
       BCol,
       BFormGroup,
       BFormInput,
+      BDropdown,
+       BDropdownItem,
       BFormCheckbox,
       BForm,
       BButton,
@@ -119,27 +178,43 @@
       if(this.id){
         this.editRow = true
         this.row = this.$store.getters.GetRowReportData(this.id)
-        console.log(this.row)
       }
+      this.agents = this.$store.getters.GetAgents 
         this.items = this.$store.getters.GetReportItems;
        
-       for(var i=0; i<this.items.length;i++){
+       for(var i=0; i < (this.items).length;i++){
             
             this.data[this.items[i].name] = ''
        }
     },
-    data(){
+    data() {
         return{
+            // for validation
+            required,
             items:[],
             data:{},
             editRow:false,
-            row:null
+            row:null,
+            agents:[],
+            agentName:null
         }
 
     },
     methods:{
+      validationForm() {
+         this.$refs.simpleRules.validate().then(success => {
+          if (success && !this.editRow) {
+            // eslint-disable-next-line
+            this.AddRow();
+          }
+          else if(success && this.editRow){
+            this.EditRow()
+          }
+        })
+      },
         AddRow(){
-            this.data['id'] = new Date()
+            this.data['id'] = new Date().toISOString().split('T')[0]
+            this.data['releaseStatus'] = false
             this.$store.commit('AddRow',this.data)
             this.$router.push('full-report')
         },
